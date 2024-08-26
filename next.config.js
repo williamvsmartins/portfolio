c72a468fb16b4db65f7669d06106e68edd640ca8
williamvsmartins <williamvaltherprogramador@gmail.com>
2024-08-26 14:58:40 -0300

    chore: add next initial config

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+ 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      }
+    ],
+  },
+}
